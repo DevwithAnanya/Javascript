60fad5eda1f69ceff62384d95811aad08177b383
DevwithAnanya <ananyasharma8983@gmail.com>
2024-05-28 19:33:28 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(3+3) we are using node , not browser
+// broswer me JS ka engine hota hai inbuilt 
+// to wahan chl jayega , node me alg way hai
+
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"  // string type
+let age = 18             //number type
+let isLoggedIn = false     //boolean
+let state;               //  undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value , absence of any object value
+// undefined => 
+// symbol =>  used to create unique keys for objects
+
+
+// object
+
+console.log(typeof undefined); // undefined   , typeof operator returns the type of the value
+console.log(typeof null); // object`  
+/*
+In the first implementation of JavaScript, JavaScript values were represented as a type tag and a value.
+ The type tag for objects was 0 Consequently, null had 0 as type tag, hence the typeof return value "object".
+*/
+
