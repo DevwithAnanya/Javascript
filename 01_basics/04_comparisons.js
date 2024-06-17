d73368b1fd24b1f8df196ea757b389261281c7b4
Ananya Trinach <ananyasharma8983@gmail.com>
2024-06-17 16:50:51 +0000

    comaprison_operators

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,9 @@
+console.log(2>3);
+console.log("2">1); //JS automatically converts string
+// to number
+console.log(null==0);  
+console.log(null>=0);
+// comparison operators convert null to 0
+console.log("2"===2); 
+// triple equal to - strict  check , it checks value as 
+// well as datatype
