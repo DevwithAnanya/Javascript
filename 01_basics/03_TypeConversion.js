12e79bbdb38a01075ad4a5782451e48b6cf5ea5f
Ananya Trinach <ananyasharma8983@gmail.com>
2025-02-02 05:25:15 +0000

    conversion of datatypes

--- 01_basics/03_TypeConversion.js
+++ 01_basics/03_TypeConversion.js
@@ -3,28 +3,29 @@ let score ="33"
 console.log(typeof score)
 // sometimes we are not sure whether user has entered
 // a number or a string , so check first
-let valueinNumber = Number(score);  //type converion
+let valueinNumber = Number(score);  //type conversion
 // to number datatype
 console.log(typeof valueinNumber)
 
 let id="33abc"
 let valueinId = Number(id)
-// id will be converted to number , but value 
-// is NaN - not a number
-console.log(typeof valueinId)
-console.log(valueinId)
+//The Number() function tries to convert the string "33abc" 
+// into a number, but it contains non-numeric characters (abc).
+//hence it returned NaN
+console.log(typeof valueinId)  //type would be number
+console.log(valueinId) //value is NaN
 
-// "33"-33
-// "33abc" - NaN
-// null -0
+// "33"->33
+// "33abc" -> NaN
+// null ->0
 // undefined -  NaN
 // true-1 , false -0
 
 // For Boolean() 
-// 1-true
-// 0-false
-// ""-false
-// "Ananya"-true
+// 1->true
+// 0->false
+// ""->false
+// "Ananya"->true
 
 let str1="hello"
 let str2= " Javascript"
