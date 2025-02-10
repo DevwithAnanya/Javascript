26d1677cc6062f6ec62996818d6acb8e810aba85
Ananya Trinach <ananyasharma8983@gmail.com>
2025-02-10 13:20:09 +0000

    truthly values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,17 @@
+// falsy value 
+// false , 0 , -0 , 0n ,"",null , undefined , NaN
+
+// truthy values 
+// "0" , 'false' , " "(space in string) , [] ,{} , function(){}(empty fxn)
+
+// checking for Array
+// if(myarr.length === 0){
+//     execute this
+// }
+
+// checking for object 
+// const myobj={}
+
+// if((Object.keys(myobj).length === 0)){   //object.keys(--) gives the keys in form of array
+//     execute this
+// }
